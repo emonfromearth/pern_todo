ef14fa070c7d5a74b5b3609403114910406f760f
Sabbir Hosen Emon <qzjaman@gmail.com>
2022-01-27 14:10:30 +0600

    delete from client done

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -2,7 +2,20 @@ import {useEffect, useState} from "react";
 
 const List = () => {
     const [todos, setTodos] = useState([]);
-    
+
+    // delete a todo
+    const handleDelete = async (id) => {
+        try {
+            await fetch(`http://localhost:4000/${id}`, {
+                method: 'DELETE'
+            });
+            setTodos(todos.filter(todo => todo.id !== id));
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    // get all todos
     const getTodos = async () => {
         try {
             const response = await fetch('http://localhost:4000/');
@@ -13,6 +26,11 @@ const List = () => {
         }
     }
 
+    // edit a todo
+    const handleEdit = async (id) => {
+        console.log(id);
+    }
+    
     useEffect(() => {
         getTodos();
     }, []);
@@ -34,8 +52,8 @@ const List = () => {
                     <td>{todo.id}</td>
                     <td>{todo.description}</td>
                     <td className="text-center">
-                        <button className="btn btn-primary">Edit</button>
-                        <button className="btn btn-danger">Delete</button>
+                        <button className="btn btn-primary" onClick={() => handleEdit(todo.id)}>Edit</button>
+                        <button className="btn btn-danger" onClick={() => handleDelete(todo.id)}>Delete</button>
                     </td>
                 </tr>
             ))}
