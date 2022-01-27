64076466cfba6011c3478ce0363a29b5c0f8757e
Sabbir Hosen Emon <qzjaman@gmail.com>
2022-01-27 17:06:46 +0600

    make it better

--- server/db.js
+++ server/db.js
@@ -3,11 +3,11 @@ const Pool = pg.Pool;
 
 // database configuration data
 const config = {
-    user: 'postgres',
-    password: 'admin',
-    host: 'localhost',
-    port: 1234,
-    database: 'perntodo',
+    user: 'zrgumgzu',
+    password: 'VZ8Tmf9HuELPO1siZM_gl9CA2AAlg7SA',
+    host: 'arjuna.db.elephantsql.com',
+    port: 5432,
+    database: 'zrgumgzu',
 };
 
 // setup database connection with config data
--- server/server.js
+++ server/server.js
@@ -64,5 +64,5 @@ app.delete('/:id', async (req, res) => {
 
 // server is running on port 5000
 app.listen(4000, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 4000');
 });
